73968427efc39934d688128f932e0ca166244753
Mykyta Bondarenko <nikbond@portaone.com>
2018-01-04 22:16:23 +0200

    Separate server to .cpp & .hpp

---
+++ MessengerServer.cpp
@@ -0,0 +1,32 @@
+#include "MessengerServer.hpp"
+
+MessengerServer::MessengerServer(size_t port)
+    : serverEndPoint(ip::tcp::v4(), port),
+      acceptor(ioService, serverEndPoint)
+{
+    std::cout << "Server is created" << std::endl;
+}
+
+void MessengerServer::run() {
+    std::cout << "Server is started on " << serverEndPoint.address().to_string()
+              << ":" << serverEndPoint.port() << std::endl;
+
+    char data[BUFFER_SIZE] = {0};
+    while (true) {
+        ip::tcp::socket socket(ioService);
+        acceptor.accept(socket);
+        size_t messageLength = socket.read_some(buffer(data));
+        if (0 != messageLength) {
+            std::cout << socket.remote_endpoint()
+                      << " > " << data << std::endl;
+
+            clients.push_back(std::move(socket));
+
+            for(auto& client: clients) {
+                client.write_some(buffer(data));
+                std::cout << client.remote_endpoint()
+                          << " < " << data << std::endl;
+            }
+        }
+    }
+}
